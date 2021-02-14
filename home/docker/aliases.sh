de40e821bbabaf0023d544737e83513d1dd376ae
Ruslan Ibragimov <ruslan@ibragimov.by>
2021-02-15 02:05:55 +0300

    Hugo container

--- home/docker/aliases.sh
+++ home/docker/aliases.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 alias hugo="docker run --rm -it -v $(pwd):/data --network=host hugo"
+alias hugo_server="docker run --rm -it -v $(pwd):/data --network=host hugo server --disableFastRender --buildFuture --bind 0.0.0.0"
