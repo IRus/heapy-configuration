6dec5c53340a064fc5894cef3473e2b2e74f0057
Ruslan Ibragimov <ruslan@ibragimov.by>
2021-02-24 18:00:29 +0300

    Update aliases.sh

--- home/docker/aliases.sh
+++ home/docker/aliases.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 function hugo() {
-    docker run --rm -it -v $(pwd):/data hugo $@
+    docker run --rm -it -v "$(pwd)":/data hugo "$@"
 }
 
 function hugo_server() {
-    docker run --rm -it -p 1313:1313 -v $(pwd):/data hugo server --disableFastRender --buildFuture --bind 0.0.0.0 $@
+    docker run --rm -it -p 1313:1313 -v "$(pwd)":/data hugo server --disableFastRender --buildFuture --bind 0.0.0.0 "$@"
 }
