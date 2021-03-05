c1e9675656b3419ab4d634663288972da93f75eb
Ruslan Ibragimov <ruslan@ibragimov.by>
2021-03-06 00:27:29 +0300

    Mtproto

--- home/docker/mtproto/run.sh
+++ home/docker/mtproto/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+echo "Secret used: [${PROXY_SECRET}]"
+
+/mtproto-proxy --help
+
 exec /mtproto-proxy -u nobody -p 8080 -H 8888 -S "${PROXY_SECRET}" --aes-pwd proxy-secret proxy-multi.conf -M 1
