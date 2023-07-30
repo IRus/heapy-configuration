3caceebc9e8088058b97f6a3043034e51a372566
Ruslan Ibragimov <ruslan@heapy.io>
2023-07-30 15:11:17 +0300

    feat: Add Sentry.io integration to backup script
    This commit adds the integration with Sentry.io to the backup script. Now, the script will start by sending a notification to Sentry.io and then finish by notifying Sentry.io when it completes successfully. This provides better monitoring and error tracking for the backup process.
    
    The backup.sh script has been modified to include the functions `sentry_start` and `sentry_finish`. The `sentry_start` function sends a POST request to Sentry.io API to update the status of the monitor check as "in_progress". The `sentry_finish` function sends a PUT request to Sentry.io API to update the status of the monitor check as "ok" after the script finishes successfully.
    
    Additionally, the script now outputs logs and sends them to /tmp/cron.log for debugging purposes.
    
    These changes improve the reliability and monitoring capabilities of the backup process.

--- birdperson/backup.sh
+++ birdperson/backup.sh
@@ -2,6 +2,27 @@
 
 set -e
 
+function sentry_start() {
+    curl -X POST \
+        'https://sentry.io/api/0/organizations/heapy/monitors/75b54c8c-7ef2-4077-9f00-adccd33841b3/checkins/' \
+        --header "Authorization: DSN ${SENTRY_DSN}" \
+        --header 'Content-Type: application/json' \
+        --data-raw '{"status": "in_progress"}'
+}
+
+function sentry_finish() {
+    CHECKIN_ID=$1
+    curl -X PUT \
+        "https://sentry.io/api/0/organizations/heapy/monitors/75b54c8c-7ef2-4077-9f00-adccd33841b3/checkins/${CHECKIN_ID}/" \
+        --header "Authorization: DSN ${SENTRY_DSN}" \
+        --header 'Content-Type: application/json' \
+        --data-raw '{"status": "ok"}'
+}
+
+echo "Starting script, send notification to sentry.io"
+CHECKIN_ID=$(sentry_start | jq -r .id)
+echo "CHECKIN_ID: ${CHECKIN_ID}"
+
 TIME=$(date "+%s")
 BACKUP_NAME="birdperson-${TIME}.tar.gz"
 BACKUP_PATH="/tmp/${BACKUP_NAME}"
@@ -14,6 +35,8 @@ sudo chown pi:pi "${BACKUP_PATH}"
 
 echo "Archive ${BACKUP_PATH} created"
 
+echo "Uploading ${BACKUP_NAME} to R2"
 rclone moveto "${BACKUP_PATH}" "r2:birdperson/${BACKUP_NAME}"
 
-echo "Archive ${BACKUP_NAME} uploaded to R2"
+echo "Notifying sentry.io that script finished"
+sentry_finish $CHECKIN_ID
