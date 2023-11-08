0a506f2f43e5d2a20ee8c76cacfa15115de82b09
Ruslan Ibragimov <ruslan@heapy.io>
2023-11-08 12:05:41 +0300

    Easier to use script

--- morty/update.sh
+++ morty/update.sh
@@ -2,9 +2,10 @@
 
 set -e
 
+cd ~/heapy-configuration/morty || exit 1
 git pull --verbose
 cp --verbose .bash_aliases ~
 cp --verbose docker-compose.yml ~/iot
-../birdperson/update.sh
+~/heapy-configuration/birdperson/update.sh
 cd ~/iot || exit 1
 docker compose up -d
