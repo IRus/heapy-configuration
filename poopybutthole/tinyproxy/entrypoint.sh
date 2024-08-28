c11d3b6cca989b65527aa54a528a739b6a04026a
Ruslan Ibragimov <ruslan@heapy.io>
2024-08-28 08:14:37 +0300

    Use wireguard for ttrss

--- poopybutthole/tinyproxy/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Set up basic auth if USERNAME and PASSWORD are provided
-if [ -n "$USERNAME" ] && [ -n "$PASSWORD" ]; then
-    echo "BasicAuth $USERNAME $PASSWORD" >> /etc/tinyproxy/tinyproxy.conf
-fi
-
-# Start Tinyproxy
-exec tinyproxy -d
