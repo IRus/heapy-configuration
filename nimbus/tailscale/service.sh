67359fa23c0b47bad9ad88ed0fa1ac1235be5e7b
Ruslan Ibrahimau <ruslan@heapy.io>
2026-06-08 20:31:50 +0300

    Add Plex discovery proxy service with socat via Tailscale

--- nimbus/tailscale/service.sh
+++ nimbus/tailscale/service.sh
@@ -2,6 +2,8 @@
 
 ### Setup proxy using socat for Plex
 
+sudo apt install -y socat
+
 sudo tee /etc/systemd/system/plex-proxy.service <<'EOF'
 [Unit]
 Description=Plex proxy via Tailscale
@@ -16,4 +18,23 @@ RestartSec=5
 WantedBy=multi-user.target
 EOF
 
+sudo tee /etc/systemd/system/plex-discovery.service <<'EOF'
+[Unit]
+Description=Plex GDM discovery proxy via Tailscale
+After=network.target tailscaled.service
+
+[Service]
+ExecStart=/usr/bin/socat UDP-LISTEN:32414,fork,reuseaddr UDP:kashyyyk:32414
+Restart=always
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
 sudo systemctl enable --now plex-proxy
+sudo systemctl enable --now plex-discovery
+
+sudo systemctl status plex-proxy
+sudo systemctl status plex-discovery
